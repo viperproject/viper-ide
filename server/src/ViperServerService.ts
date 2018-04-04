8cd3e6784da30cbfa0a465789d5a1813e1ed40e0
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2018-04-04 15:46:48 +0200

    Properly treat overall verification results from custom verification backends. Assume custom backends do not support fine-grained (e.g., per-method) reporting.

--- server/src/ViperServerService.ts
+++ server/src/ViperServerService.ts
@@ -220,6 +220,8 @@ export class ViperServerService extends BackendService {
 
                                 let first_error_tag = message.msg_body.details.result.errors[0].tag
                                 let global_failure = 
+                                    Server.backend.type === 'other' || 
+                                    /* TODO: implement Implement flag DoesCustomBackendSupportFineGrainedLogging */
                                     message.msg_body.verifier === 'carbon' ||
                                     first_error_tag === 'parser.error' || 
                                     first_error_tag === 'parser.warning' ||
