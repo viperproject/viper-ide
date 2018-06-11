0ca5486024cee61e02cf2f5448f9f66e7c4a08a2
Alessio Aurecchia <alessio@aurecchia.ch>
2018-06-11 11:52:04 +0200

    Cleanup webview panel code

--- debugger/src/panel/panel.ts
+++ debugger/src/panel/panel.ts
@@ -18,19 +18,21 @@ const JsonFormatConfiguration: JSONFormatterConfiguration = {
 function activate() {
     Logger.debug("Setting up debug pane");
 
-    // TODO: Better ways to handle this?
-    var e = document.getElementById("output");
-    if (!e) {
-        Logger.error("Could not find the output div, exiting");
-        return;
-    } else {
-        outpudDiv = e;
-    }
+    setupPanelSplits();
+    setupMessageHandlers();
+    setupInputHandlers();
+    setupGraph();
 
+    Logger.debug("Done setting up debug pane");
+}
+
+
+/** Sets up the splits in the debug pane.  */
+function setupPanelSplits() {
     let panels: HTMLElement[] = $('.panel').toArray();
 
+    // Determine how many panels are opened by default, so we can compute the size of each open panel
     let isCollapsed = panels.map(e => e.classList.contains('collapsedByDefault'));
-    console.log(panels.map(e => e.classList));
     // This is basically a fold
     let numberOfCollapsedPanels = isCollapsed.reduce((tot, collapsed) => collapsed ? tot + 1 : tot, 0);
     let percentForOpenPanel = 100 / (panels.length - numberOfCollapsedPanels);
@@ -44,11 +46,34 @@ function activate() {
         minSize: 0,
         snapOffset: 60,  // When a panel is less than this, it closes
     });
+}
 
-    setupMessageHandlers();
-    setupButtonHandlers();
 
-    outpudDiv.innerHTML += "<p>Viper Debugger Started</p>";
+/** Sets up the handlers for messages coming from the extension. */
+function setupMessageHandlers() {
+    Logger.debug("Setting up message handlers");
+
+    // Helper function for setting callbacks
+    function on(key: string, callback: (message: any) => void) {
+        window.addEventListener('message', e => {
+            let message = e.data;
+            if (message.type === key) {
+                callback(message);
+            }
+        });
+    }
+
+    on('logMessage', message => handleOutputMessage(message));
+    on('stateUpdate', message => handleStateUpdate(message));
+    on('verifiables', message => handleVerifiableUpdate(message.data));
+
+    Logger.debug("Done setting up message handlers.");
+}
+
+
+/** Sets up handlers for button events in the debugger pane. */
+function setupInputHandlers() {
+    Logger.debug("Setting up input handlers.");
 
     // TODO: Proper key handling
     $(document).keydown(function(e) {
@@ -62,6 +87,25 @@ function activate() {
         }
     });
 
+    // Send navigation actions
+    // The message is delivered to the DebuggerSession via the DebuggerPanel, on "the extension side"
+    $('#next:button').click(() => vscode.postMessage({ command: 'nextState' }));
+    $('#previous:button').click(() => vscode.postMessage({ command: 'previousState' }));
+    $('#child:button').click(() => vscode.postMessage({ command: 'childState' }));
+    $('#parent:button').click(() => vscode.postMessage({ command: 'parentState' }));
+
+    // Enable/disable state navigation via mouse
+    // The message is delivered to the DecorationsManager via the DebuggerPanel, on "the extension side"
+    $('#mouseNavigation').change((event) => {
+        let input = $(event.currentTarget);
+        vscode.postMessage({ command: 'mouseNavigation', value: input.prop('checked')});
+    });
+
+    Logger.debug("Done setting up input handlers.");
+}
+
+
+function setupGraph() {
     // var d3 = require('d3');
     // var d3graphviz = require('d3-graphviz');
 
@@ -82,75 +126,22 @@ function activate() {
     //     d3.select(this)
     //         .attr("transform", `translate(${d3.event.x},${d3.event.y})`);
     // }
-
-    Logger.debug("Done setting up debug pane");
-}
-
-function clear() {
-    $('#currentState').empty();
-}
-
-/** Sets up the handlers for messages coming from the extension. */
-function setupMessageHandlers() {
-    Logger.debug("Setting up message handlers");
-
-    function on(key: string, callback: (message: any) => void) {
-        window.addEventListener('message', e => {
-            let message = e.data;
-            if (message.type === key) {
-                callback(message);
-            }
-        });
-    }
-
-    on('logMessage', message => outpudDiv.innerHTML += "<p>" + message.text + "</p>");
-    on('stateUpdate', (message) => state(message));
-    on('verifiables', (message) => {
-        const verifiables: any[] = message.data;
-        const options = verifiables.map( (v) => $('<option />')
-                                                    .text(v.name)
-                                                    .attr('value', v.name) );
-        const dropdown = $('#verifiables');
-        const selected = dropdown.val();
-        dropdown.empty();
-
-        // Handler for the selection change
-        dropdown.change((event) => { 
-            const name = $('#verifiables').val();
-            vscode.postMessage({ command: 'selectVerifiable', data: name });
-        });
-
-        if (options.length > 0) {
-            dropdown.append(options);
-            dropdown.prop('disabled', false);
-
-            // Re-select the previously selected verifiable if possible
-            let elem = options.find(e => e.val() === selected);
-            if (elem) {
-                dropdown.val(elem.val()!);
-            } else {
-                // Trigger updating panel to the first verifiable
-                options[0].change();
-            }
-        } else {
-            dropdown.prop('disabled', true);
-            clear();
-        }
-    });
-
-    Logger.debug("Done setting up message handlers.");
 }
 
 
-function state(message: any) {
+/** Handles the change of current state being debugged. */
+function handleStateUpdate(message: any) {
+    // TODO: Clean this up.
     const state = message.data.current;
     const stateDiv = $('#currentState');
     stateDiv.empty();
 
     if (!state) {
+        Logger.error(`Received state update message with no state: '${message}'`);
         return;
     }
 
+    // Add state type to the panel
     if (state.type && state.type !== 'None') {
         stateDiv.removeClass();
         stateDiv.addClass(state.type.toLowerCase());
@@ -163,12 +154,14 @@ function state(message: any) {
         stateDiv.append(elem);
     }
 
+    // Enable/disable navigation buttons according to parent/siblings/children
     let data = message.data;
     $('button#next').prop('disabled', !data.hasNext);
     $('button#previous').prop('disabled', !data.hasPrevious);
     $('button#parent').prop('disabled', !data.hasParent);
     $('button#child').prop('disabled', !data.hasChild);
     
+    // Update the JSON view of the state tree
     const openLevel = 4;
     const current = new JSONFormatter(state, openLevel, JsonFormatConfiguration);
     const pre = $('<pre></pre>').append(current.render());
@@ -176,23 +169,45 @@ function state(message: any) {
 }
 
 
-/** Sets up handlers for button events in the debugger pane. */
-function setupButtonHandlers() {
-    Logger.debug("Setting up button handlers.");
+/** Handles the message containing new verifiables for this session. */
+function handleVerifiableUpdate(verifiables: any[]) {
+    const options = verifiables.map((v) => $('<option />').text(v.name)
+                                                          .attr('value', v.name));
+    const dropdown = $('#verifiables');
+    const selected = dropdown.val();
+    dropdown.empty();
 
-    $('#next:button').click(() => vscode.postMessage({ command: 'nextState' }));
-    $('#previous:button').click(() => vscode.postMessage({ command: 'previousState' }));
-    $('#child:button').click(() => vscode.postMessage({ command: 'childState' }));
-    $('#parent:button').click(() => vscode.postMessage({ command: 'parentState' }));
-
-    $('#mouseNavigation').change((event) => {
-        let input = $(event.currentTarget);
-        vscode.postMessage({ command: 'mouseNavigation', value: input.prop('checked')});
+    // Setup handler for the selection change
+    dropdown.change((event) => { 
+        const name = $('#verifiables').val();
+        vscode.postMessage({ command: 'selectVerifiable', data: name });
     });
 
-    Logger.debug("Done setting up button handlers.");
+    // Set the options, if any
+    if (options.length > 0) {
+        dropdown.append(options);
+        dropdown.prop('disabled', false);
+
+        // Re-select the previously selected verifiable if possible
+        let elem = options.find(e => e.val() === selected);
+        if (elem) {
+            dropdown.val(elem.val()!);
+        } else {
+            // Trigger updating panel to the first verifiable
+            options[0].change();
+        }
+    } else {
+        dropdown.prop('disabled', true);
+        $('#currentState').empty();
+    }
 }
 
 
-// Set up the debugger pane
+/** Handles messages being logged to the output split in the debug pane. */
+function handleOutputMessage(message: any) {
+    $("#output").append($("<p></p>").text(message.text));
+}
+
+
+// Start up the debugger pane
 activate();
