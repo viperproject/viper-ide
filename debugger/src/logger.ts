befea5f10d9aff6ef348a7d7907ebe59d2eb9c7a
Alessio Aurecchia <alessio@aurecchia.ch>
2018-07-20 18:32:00 +0200

    Log stuff to an output feed in VS Code.

--- debugger/src/logger.ts
+++ debugger/src/logger.ts
@@ -1,21 +1,47 @@
+import * as vscode from "vscode";
+
+
+export enum LogLevel {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+}
 
 
 // TODO: Implement proper logging
 export class Logger {
 
+    private static outputChannel = vscode.window.createOutputChannel("Viper Debugger");
+    private static logLevel: LogLevel = LogLevel.INFO;
+
+    private static log(message: string, level: LogLevel) {
+        if (level >= Logger.logLevel) {
+            Logger.outputChannel.appendLine(message);
+        }
+    }
+
+    public static setLogLevel(logLevel: LogLevel) {
+        if (logLevel > LogLevel.WARNING) {
+            vscode.window.showWarningMessage(
+                `Setting Viper Debugger log level to ${logLevel}, warning messages will not be printed.`
+            );
+        }
+    }
+
     public static info(message: string): void {
-        console.log(message);
+        Logger.log(message, LogLevel.INFO);
     }
 
     public static debug(message: string): void {
-        console.log(message);
+        Logger.log(message, LogLevel.DEBUG);
     }
 
     public static error(message: string): void {
-        console.error(message);
+        Logger.log('[ERROR] ' + message, LogLevel.ERROR);
     }
 
     public static warn(message: string): void {
-        console.log('WARN! ' + message);
+        Logger.log('[WARNING] ' + message, LogLevel.WARNING);
     }
 }
