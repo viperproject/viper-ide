49a19895079afb135d0d4c40d7f026fff578ed3c
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2026-02-12 11:25:36 +0100

    Change to beginner mode

--- client/src/Settings.ts
+++ client/src/Settings.ts
@@ -942,10 +942,10 @@ export class Settings {
 
         const configuredArgString = Settings.getConfiguration("viperServer").customArguments;
         const useBackendSpecificCache = Settings.getConfiguration("viperServer").backendSpecificCache === true;
-        const presentationMode = Settings.getConfiguration("viperServer").presentationMode === true;
+        const beginnerMode = Settings.getConfiguration("viperServer").beginnerMode === true;
         return configuredArgString
             .replace("$backendSpecificCache$", useBackendSpecificCache ? "--backendSpecificCache" : "")
-            .replace("$presentationMode$", presentationMode ? "--presentationMode" : "")
+            .replace("$beginnerMode$", beginnerMode ? "--beginnerMode" : "")
             .replace("$logLevel$", convertLogLevel(logLevel))
             // note that we use functions as 2nd argument since we do not want that
             // the special replacement patterns kick in
